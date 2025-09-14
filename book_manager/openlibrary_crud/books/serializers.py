8d1658d035cf92104b45adae15f463b7af94bf73
EuroBoyy#55 <hleodymann@gmail.com>
2025-09-14 01:24:45 -0300

    :arrow_up: feat: criei Serializer com todos os campos do Model Book para API

---
+++ book_manager/openlibrary_crud/books/serializers.py
@@ -0,0 +1,9 @@
+# books/serializers.py
+
+from rest_framework import serializers
+from .models import Book
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'author', 'published_date', 'isbn', 'pages', 'cover_url', 'language']
